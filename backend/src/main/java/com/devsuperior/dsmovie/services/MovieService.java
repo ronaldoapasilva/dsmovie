6e4c9aa173300eb1731a0bf89f3ee9bd91f28c0e
Ronaldo Ap. Silva <sinuca@gmail.com>
2022-03-12 00:34:37 -0300

    dto

--- backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
+++ backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -30,7 +30,7 @@ public class MovieService {
 	@Transactional(readOnly = true)
 	public MovieDTO findById(Long id){
 		Movie result = repository.findById(id).get();		
-		MovieDTO dto = new MovieDTO();
+		MovieDTO dto = new MovieDTO(result);
 		
 		return dto;
 	}
