2a5783e4d00e8614a2a91cb589ece574e02b54b0
Ronaldo Ap. Silva <sinuca@gmail.com>
2022-03-13 15:39:17 -0300

    retirando avaliação fixa do movie

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -33,15 +33,7 @@ function Listing() {
     }, [pageNumber]);
 
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
-
-
+  
     const handlePageChance = (newPageNumber : number ) => {
         setPageNumber(newPageNumber);
     }
