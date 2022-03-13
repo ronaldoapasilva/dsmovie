d132be9833567680945f478faeb64f29a4da7831
Ronaldo Ap. Silva <sinuca@gmail.com>
2022-03-13 14:05:18 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,21 +1,45 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/request";
 
 
 function Listing() {
+
+const [ pageNumber, setPageNumber ] = useState(0);
+
+useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=1`)
+    .then(response => {
+        const data = response.data as MoviePage;
+        console.log(data);
+        setPageNumber(data.number);        
+    });
+}, []);
+
+//Hook: useState
+//Manter estado no componente -> 
+
+//Hook: useEffect
+//Executar algo na instanciação ou destruição do componente, observar estado
+
 //FORMA ERRADA
 
 // axios.get('http://localhost:8080/movies?size=12')
-axios.get(`${BASE_URL}/movies?size=12`)
-    .then(response => {
-        console.log(response.data);
+// axios.get(`${BASE_URL}/movies?size=12`)
 
-    });
+//axios.get(`${BASE_URL}/movies?size=12`)
+    //.then(response => {
+      //  const data = response.data as MoviePage;
+        //setPageNumber(data.number);
+        //console.log(response.data);
+    //});
 
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
